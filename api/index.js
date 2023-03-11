03c2931424367da73db46af1f2e4a865f84cf12e
Ming-der Wang <mingderwang@gmail.com>
2023-03-11 13:58:57 +0800

    support CORS

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const micro = require('micro');
 
 async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method === 'POST') {
     try {
       const body = await micro.json(req);
